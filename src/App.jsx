1e55c1adf7f09d93ace20946a1764392e65e1b37
AsthaKirar <asthap602@gmail.com>
2024-10-22 22:25:49 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux"
+
+
+
+const App = () => {
+  const data = useSelector((state)=>state);
+  console.log(data);
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
